b361d6074260287a5959fabb641dae1235aa0825
jacque <jacquelinezhang92@gmail.com>
2021-10-18 17:47:20 +1000

    2020->2021

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,60 +1,60 @@
-import React from 'react';
-import { information } from '../content/data';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { information } from "../content/data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLinkedin,
   faInstagram,
   faGithub,
-} from '@fortawesome/free-brands-svg-icons';
+} from "@fortawesome/free-brands-svg-icons";
 
 const Information = () => {
   return (
-    <div className=''>
-      <div className='pt-8 xl:pt-2 border-t xl:border-t-0'>
+    <div className="">
+      <div className="pt-8 xl:pt-2 border-t xl:border-t-0">
         <img
-          className='w-40 2xl:w-48 rounded-full mx-auto xl:ml-0 '
+          className="w-40 2xl:w-48 rounded-full mx-auto xl:ml-0 "
           src={`${process.env.PUBLIC_URL}/images/${information.image}`}
-          alt='profile'
+          alt="profile"
         ></img>
       </div>
 
-      <div className='font-bold text-3xl pt-12 xl:pt-16 uppercase'>
+      <div className="font-bold text-3xl pt-12 xl:pt-16 uppercase">
         {information.name}
       </div>
-      <div className='gray-text pt-2 pb-7'>{information.email}</div>
+      <div className="gray-text pt-2 pb-7">{information.email}</div>
 
-      <div className='py-6 border-t border-b'>
+      <div className="py-6 border-t border-b">
         {information.keywords.map((i, key) => (
-          <div key={key} className='pt-1 capitalize'>
+          <div key={key} className="pt-1 capitalize">
             {i}
           </div>
         ))}
       </div>
 
-      <div className='py-5'>
-        <div className='justify-center xl:justify-start flex mt-4'>
-          <a href={information.github.link} target='_blank' rel='noreferrer'>
+      <div className="py-5">
+        <div className="justify-center xl:justify-start flex mt-4">
+          <a href={information.github.link} target="_blank" rel="noreferrer">
             <FontAwesomeIcon
               icon={faGithub}
-              className='text-gray-400 text-2xl mr-2'
+              className="text-gray-400 text-2xl mr-2"
             />
           </a>
-          <a href={information.instagram.link} target='_blank' rel='noreferrer'>
+          <a href={information.instagram.link} target="_blank" rel="noreferrer">
             <FontAwesomeIcon
               icon={faInstagram}
-              className='text-gray-400 text-2xl mx-2'
+              className="text-gray-400 text-2xl mx-2"
             />
           </a>
           <a href={information.linkedin.link}>
             <FontAwesomeIcon
               icon={faLinkedin}
-              className='text-gray-400 text-2xl ml-2'
+              className="text-gray-400 text-2xl ml-2"
             />
           </a>
         </div>
 
-        <p className='copyright text-gray-400 text-sm mt-4'>
-          &copy; Kai Tuo 2020
+        <p className="copyright text-gray-400 text-sm mt-4">
+          &copy; Kai Tuo 2021
         </p>
       </div>
     </div>
